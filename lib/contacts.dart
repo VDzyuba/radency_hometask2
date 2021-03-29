4e25025e57fde8762c49435442e3b6c7d9d592d9
VDzyuba28 <dzyubavi98@gmail.com>
2021-03-29 20:55:32 +0300

    Added main functional

---
+++ lib/contacts.dart
@@ -0,0 +1,43 @@
+ final contacts = [
+    {
+      "firstName": "Elon",
+      "lastName": "Musk",
+      "company": "Tesla",
+      "images": "elon-musk.jpg"
+    },
+    {
+      "firstName": "Steve",
+      "lastName": "Mask",
+      "company": "Tesla",
+      "images": "elon-musk.jpg"
+    },
+    {
+      "firstName": "Elon",
+      "lastName": "Miisk",
+      "company": "Tesla",
+      "images": "elon-musk.jpg"
+    },
+    {
+      "firstName": "Steve",
+      "lastName": "Jobs",
+      "company": "Apple",
+      "images": "steve-jobs.jpg"
+    },
+    {
+      "firstName": "Bill",
+      "lastName": "Gates",
+      "company": "Microsoft",
+      "images": "bill-gates.jpg"
+    },
+    {
+      "firstName": "Bill",
+      "lastName": "Bates",
+      "company": "Microsoft",
+      "images": "bill-gates.jpg"
+    },
+    {
+      "firstName": "Steve",
+      "lastName": "Weebs",
+      "company": "Apple",
+    },
+  ];
